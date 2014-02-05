d882f1032afaf59528cf29f3cae91b7f1aecdd80
shashank singh <shashank.personal@gmail.com>
2014-02-05 13:46:43 +0530

    added a background

--- android/gen/com/shashanksingh/video_streamer/R.java
+++ android/gen/com/shashanksingh/video_streamer/R.java
@@ -22,6 +22,7 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int slqsm=0x7f020001;
     }
     public static final class id {
         public static final int action_settings=0x7f080007;
@@ -29,8 +30,8 @@ public final class R {
         public static final int editText2=0x7f080005;
         public static final int linearLayout1=0x7f080000;
         public static final int login=0x7f080002;
-        public static final int play=0x7f080001;
-        public static final int signup=0x7f080003;
+        public static final int play=0x7f080003;
+        public static final int signup=0x7f080001;
         public static final int textView1=0x7f080006;
     }
     public static final class layout {
