ea6cf9197b9ac9feade9650091ea3864019f8c04
shashank singh <shashank.personal@gmail.com>
2014-02-05 14:17:59 +0530

    trying to see which formats android will play natively

--- android/src/com/shashanksingh/video_streamer/play.java
+++ android/src/com/shashanksingh/video_streamer/play.java
@@ -1,7 +1,10 @@
 package com.shashanksingh.video_streamer;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class play extends Activity {
 
@@ -10,7 +13,14 @@ public class play extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.play);
-	    // TODO Auto-generated method stub
+	    String URL ="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4";
+	    Uri uri = Uri.parse(URL); //Declare your url here.
+
+	    VideoView mVideoView  = (VideoView)findViewById(R.id.videoView1);
+	    mVideoView.setMediaController(new MediaController(this));       
+	    mVideoView.setVideoURI(uri);
+	    mVideoView.requestFocus();
+	    mVideoView.start();
 	}
 
 }
