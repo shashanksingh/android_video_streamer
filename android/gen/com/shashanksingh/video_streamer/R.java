86bd9a9e16c972b1c667a706e709a77d034c13ad
shashank singh <shashank.personal@gmail.com>
2014-02-06 18:12:51 +0530

    fixing stuff in activity , coudlnt add fb login so experimentign with google plus auth

--- android/gen/com/shashanksingh/video_streamer/R.java
+++ android/gen/com/shashanksingh/video_streamer/R.java
@@ -28,9 +28,9 @@ public final class R {
         public static final int action_settings=0x7f080008;
         public static final int editText1=0x7f080004;
         public static final int editText2=0x7f080005;
-        public static final int linearLayout1=0x7f080000;
         public static final int login=0x7f080002;
         public static final int play=0x7f080003;
+        public static final int sign_in_button=0x7f080000;
         public static final int signup=0x7f080001;
         public static final int textView1=0x7f080006;
         public static final int videoView1=0x7f080007;
--- android/src/com/shashanksingh/video_streamer/play.java
+++ android/src/com/shashanksingh/video_streamer/play.java
@@ -3,6 +3,7 @@ package com.shashanksingh.video_streamer;
 import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -13,8 +14,12 @@ public class play extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.play);
-	    String URL ="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4";
-	    Uri uri = Uri.parse(URL); //Declare your url here.
+	    //String SD_CARD_PATH = Environment.getExternalStorageDirectory().toString();
+	    //String URL = SD_CARD_PATH + "627.mp4";
+	    //String URL = "http://www.pocketjourney.com/downloads/pj/video/famous.3gp";
+	    String URL = "http://hermes.sprc.samsung.pl/widget/tmp/testh.3gp";
+
+	    Uri uri = Uri.parse(URL); //Declare your Url here.
 
 	    VideoView mVideoView  = (VideoView)findViewById(R.id.videoView1);
 	    mVideoView.setMediaController(new MediaController(this));       
