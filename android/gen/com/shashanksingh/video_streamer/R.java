89bd424f83fb2bb529891a4bcb9ed39c264f5eac
shashank singh <shashank.personal@gmail.com>
2014-02-11 20:25:05 +0530

    mroe fixes to name

--- android/gen/com/shashanksingh/video_streamer/R.java
+++ android/gen/com/shashanksingh/video_streamer/R.java
@@ -60,14 +60,17 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int bollywood=0x7f050003;
+        public static final int email=0x7f05000f;
         public static final int hello_world=0x7f050002;
         public static final int info=0x7f050004;
         public static final int khabar=0x7f050005;
         public static final int login=0x7f050008;
         public static final int logo=0x7f05000c;
         public static final int news=0x7f050006;
+        public static final int password=0x7f05000e;
         public static final int play=0x7f050007;
         public static final int signup=0x7f050009;
+        public static final int submit=0x7f05000d;
         public static final int watchlive=0x7f05000b;
         public static final int youarewatching=0x7f05000a;
     }
--- android/src/com/shashanksingh/video_streamer/play.java
+++ android/src/com/shashanksingh/video_streamer/play.java
@@ -1,10 +1,8 @@
 package com.shashanksingh.video_streamer;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
