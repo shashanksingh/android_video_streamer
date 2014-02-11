00df1b76d6219ecb7230247397bc71520f55acfc
shashank singh <shashank.personal@gmail.com>
2014-02-11 20:10:10 +0530

    adding strings

--- android/gen/com/shashanksingh/video_streamer/R.java
+++ android/gen/com/shashanksingh/video_streamer/R.java
@@ -59,7 +59,14 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int bollywood=0x7f050003;
         public static final int hello_world=0x7f050002;
+        public static final int info=0x7f050004;
+        public static final int khabar=0x7f050005;
+        public static final int login=0x7f050008;
+        public static final int news=0x7f050006;
+        public static final int play=0x7f050007;
+        public static final int signup=0x7f050009;
     }
     public static final class style {
         /** 
