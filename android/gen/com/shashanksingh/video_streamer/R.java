8a1bfb1e96eeb8bb9a04e08cc5abf55cbe30e66e
shashank singh <shashank.personal@gmail.com>
2014-02-12 12:53:43 +0530

    fixing main scree

--- android/gen/com/shashanksingh/video_streamer/R.java
+++ android/gen/com/shashanksingh/video_streamer/R.java
@@ -22,27 +22,33 @@ public final class R {
     }
     public static final class drawable {
         public static final int bollywood=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int info=0x7f020002;
-        public static final int khabar=0x7f020003;
-        public static final int logo=0x7f020004;
-        public static final int news=0x7f020005;
-        public static final int slqsm=0x7f020006;
+        public static final int bollywood_small=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int info=0x7f020003;
+        public static final int info_small=0x7f020004;
+        public static final int khabar=0x7f020005;
+        public static final int khabar_small=0x7f020006;
+        public static final int logo=0x7f020007;
+        public static final int logo_small=0x7f020008;
+        public static final int logo_transparent=0x7f020009;
+        public static final int news=0x7f02000a;
+        public static final int news_small=0x7f02000b;
+        public static final int slqsm=0x7f02000c;
     }
     public static final class id {
         public static final int action_settings=0x7f08000f;
         public static final int bollywood=0x7f08000d;
         public static final int email=0x7f080006;
-        public static final int imageView1=0x7f080000;
+        public static final int imageView1=0x7f080003;
         public static final int info=0x7f08000b;
         public static final int khabar=0x7f08000e;
-        public static final int login=0x7f080004;
+        public static final int login=0x7f080001;
         public static final int news=0x7f08000c;
         public static final int password=0x7f080008;
         public static final int play=0x7f080002;
-        public static final int signup=0x7f080003;
+        public static final int signup=0x7f080000;
         public static final int submit=0x7f080009;
-        public static final int textView1=0x7f080001;
+        public static final int textView1=0x7f080004;
         public static final int textView2=0x7f080005;
         public static final int textView3=0x7f080007;
         public static final int videoView1=0x7f08000a;
