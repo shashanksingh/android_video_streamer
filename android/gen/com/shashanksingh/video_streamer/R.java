80708af7c3012fc665e1fa14625e246e0879deee
shashank singh <shashank.personal@gmail.com>
2014-02-12 19:41:45 +0530

    made main screen scollable

--- android/gen/com/shashanksingh/video_streamer/R.java
+++ android/gen/com/shashanksingh/video_streamer/R.java
@@ -42,10 +42,10 @@ public final class R {
         public static final int imageView1=0x7f080000;
         public static final int info=0x7f08000c;
         public static final int khabar=0x7f08000f;
-        public static final int login=0x7f080002;
+        public static final int login=0x7f080001;
         public static final int news=0x7f08000d;
         public static final int password=0x7f080008;
-        public static final int play=0x7f080001;
+        public static final int play=0x7f080002;
         public static final int progressbar=0x7f08000b;
         public static final int signup=0x7f080003;
         public static final int submit=0x7f080009;
