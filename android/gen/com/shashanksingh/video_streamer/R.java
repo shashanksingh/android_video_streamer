f0d9c27b373177b293da97c4aa1745efc325901a
shashank singh <shashank.personal@gmail.com>
2014-02-24 21:13:10 +0530

    icons

--- android/gen/com/shashanksingh/video_streamer/R.java
+++ android/gen/com/shashanksingh/video_streamer/R.java
@@ -23,18 +23,14 @@ public final class R {
     public static final class drawable {
         public static final int bluebackgroundtwo=0x7f020000;
         public static final int bollywood=0x7f020001;
-        public static final int bollywood_small=0x7f020002;
-        public static final int channelid=0x7f020003;
-        public static final int events=0x7f020004;
-        public static final int events_small=0x7f020005;
-        public static final int ic_launcher=0x7f020006;
-        public static final int khabar=0x7f020007;
-        public static final int khabar_small=0x7f020008;
-        public static final int logo=0x7f020009;
-        public static final int logo_small=0x7f02000a;
-        public static final int logo_transparent=0x7f02000b;
-        public static final int news=0x7f02000c;
-        public static final int news_small=0x7f02000d;
+        public static final int channelid=0x7f020002;
+        public static final int events=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int khabar=0x7f020005;
+        public static final int logo=0x7f020006;
+        public static final int logo_small=0x7f020007;
+        public static final int logo_transparent=0x7f020008;
+        public static final int news=0x7f020009;
     }
     public static final class id {
         public static final int action_settings=0x7f080014;
